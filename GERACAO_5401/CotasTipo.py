7d8b0473d528a571b375f834f14cbd8fdf3191ee
Thiago Conceição <thiago.conceicao@oliveiratrust.com.br>
2024-07-29 19:22:14 -0300

    feat: inclusão de classe responsável por atribuir números de cota as cotas de cada um dos fundos

---
+++ GERACAO_5401/CotasTipo.py
@@ -0,0 +1,62 @@
+
+'''classe responsável por definir a tipagem de cotas entre os fundos'''
+
+from pymongo import MongoClient
+import pandas as pd
+
+
+
+class CotasTipo():
+
+    ''' Classe que realiza a criação e definição dos tipos de cota para cada fundo
+        deve receber apenas as classes de cota que possuem posição ,
+         para classificar os tipos de acordo com o proposto no 54011
+    '''
+
+    def __init__(self ,  CNPJ_EMISSOR):
+        self.client = MongoClient('localhost', 27017)
+        self.cnpj_emissor = CNPJ_EMISSOR
+
+
+
+    def atribuir_tipos_de_cota(self, df):
+        '''função que define os tipos de cada uma das cotas'''
+        tipos_de_cota = []
+        dataframe_ajustado = []
+        cotas_trocaveis = ['2' , '3' , '4' , '5']
+
+        for item in df.to_dict("records"):
+            if item['tipo'] not in tipos_de_cota:
+                tipos_de_cota.append(item['tipo'])
+
+
+        for item in tipos_de_cota:
+            if item in cotas_trocaveis:
+                df_cota = df[df['tipo'] == item]
+                contagem = 1
+                for item in df_cota.to_dict("records"):
+
+                    item['tipo'] = f"{item['tipo']}.{str(contagem).zfill(2)}"
+                    dataframe_ajustado.append(item)
+                    contagem += 1
+            else:
+                dataframe_ajustado.append(item)
+
+
+        return pd.DataFrame.from_dict(dataframe_ajustado)
+
+
+
+
+    def buscar_cotas(self):
+        cotas = self.client['informes_legais']['fundos'].find({"cnpj": self.cnpj_emissor})
+        df = pd.DataFrame(cotas)
+        novo_df = self.atribuir_tipos_de_cota(df)
+
+        return novo_df
+
+
+
+
+
+
