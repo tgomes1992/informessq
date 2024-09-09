79d4881a46ad99f1973e0e3810932f8396ed38a9
Thiago Conceição <thiago.conceicao@oliveiratrust.com.br>
2024-09-09 16:17:49 -0300

    fix -> script responsável por consolidar as posições

---
+++ consolidadorposicoes.py
@@ -0,0 +1,20 @@
+from InformesLegais.Controllers.ConsolidadordePosicoes import ControllerConsolidaPosicoes
+from pymongo import MongoClient
+from multiprocessing import Pool
+import numpy as np
+
+
+
+controller = ControllerConsolidaPosicoes()
+
+client = MongoClient('localhost', 27017)
+db = client['informes_legais']
+
+fundos = db['fundos'].find({})
+codigos = [fundo['codigo'] for fundo in fundos]
+
+
+controller.get_posicoesjcot(codigos)
+
+
+
