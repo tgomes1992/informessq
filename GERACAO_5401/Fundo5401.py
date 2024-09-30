7a46822ce7fbff080fa824e00cb3c3e87784d8cd
Thiago Conceição <thiago.conceicao@oliveiratrust.com.br>
2024-09-30 20:11:59 -0300

    fix- > Ajustes na geração do informe

--- GERACAO_5401/Fundo5401.py
+++ GERACAO_5401/Fundo5401.py
@@ -79,21 +79,21 @@ class Fundo5401():
             tipos_de_cotista_O2 = {'NÃO CLASSIFICADO': 0, 'PESSOA FISICA NÃO RESIDENTE': 3, 'PESSOA FISICA PARAISO FISCAL': 3,
                                    'INSTITUIÇÃO FINANCEIRA': 3, 'PESSOA JURIDICA SEM FINS LUCRATIVOS': 4,
                                    'PESSOA JURIDICA NÃO RESIDENTE': 4, 'PESSOA JURIDICA PARAISO FISCAL': 4,
-                                   'FUNDO DE INVESTIMENTO': 7, 'FUNDO DE INVESTIMENTO IMOBILIÁRIO': 8,
+                                   'FUNDO DE INVESTIMENTO': 2, 'FUNDO DE INVESTIMENTO IMOBILIÁRIO': 2,
                                    'PESSOA FISICA': 1, 'PESSOA JURIDICA': 2,
-                                   'SOCIEDADE LTDA, ECON. MISTA, ANON., POR COTAS, ETC': 11,
+                                   'SOCIEDADE LTDA, ECON. MISTA, ANON., POR COTAS, ETC': 2,
                                    'BOLSA DE VALORES E MERCADORIAS': 12, 'CONDOMINIO': 13,
                                    'COOPERATIVA': 14, 'CONSORCIO': 15, 'INST. DE CARATER FILANTROPICO, RECREATIVO, CULT.': 16,
                                    'BANCO COMERCIAL/ MULTIPLO': 17, 'BANCO DE INVESTIMENTO': 18,
                                    'SOCIEDADE DE SEGURO, PREVIDENCIA E CAPITALIZAÇÃO': 19,
-                                   'SOCIEDADE DTVM': 20, 'SOCIEDADE CTVM E CAMBIO': 21,
+                                   'SOCIEDADE DTVM': 20, 'SOCIEDADE CTVM E CAMBIO': 2,
                                    'SOCIEDADE FINANCEIRA E DE CREDITO IMOBILIARIO': 22,
                                    'SOCIEDADE DE ARRENDAMENTO MERCANTIL (LEASING)': 23,
-                                   'FUNDO DE PREVIDENCIA PRIVADA – ABERTO': 24,
+                                   'FUNDO DE PREVIDENCIA PRIVADA – ABERTO': 2,
                                    'FUNDO DE PREVIDENCIA PRIVADA – FECHADO': 25, 'CLUBE DE INVESTIMENTO': 26, 'TEMPLO DE QUALQUER CULTO': 27,
                                    'ENTIDADE SINDICAL DOS TRABALHADORES': 28, 'DEPOSITARIO DE ADR': 29,
                                    'FUNDO DE PLANO DE BENEF. DE SOCIEDADE SEGURADORA': 30,
-                                   'UNIAO, EST., MUNIC. OU DIST. FED., AUTAR. OU FUND.': 31, 'PARTIDO POLITICO E SUAS FUNDACOES': 32}
+                                   'UNIAO, EST., MUNIC. OU DIST. FED., AUTAR. OU FUND.': 2, 'PARTIDO POLITICO E SUAS FUNDACOES': 32}
 
             return str(tipos_de_cotista_O2[investidor_cadastrado['nomePerfilTributarioInvestidor']])
         except Exception as e :
@@ -110,13 +110,12 @@ class Fundo5401():
     #todo criar função para validar o tipo de cotista e a sua respectiva classificação
         
         #todo lógica de validação do tipo de cotista
-    
-    
+
         try:
             dados_formatado = self.formatar_cotista(cotista["cpfcnpjCotista"])
             cotista_elemento = ET.Element("cotista")
 
-            cotista_elemento.set("tipoPessoa", self.get_tipo_cotista_5401(dados_formatado))
+            cotista_elemento.set("tipoPessoa", cotista['tipoCotista'])
 
             cotista_elemento.set("identificacao", dados_formatado)
 
@@ -152,7 +151,14 @@ class Fundo5401():
         fundo.set("plFundo",plFundo)
 
         return fundo
-
+        # return {
+        #     "cnpjFundo" :  cnpj_fundo ,
+        #     "quantidadeCotas": quantidade_cotas,
+        #     "quantidadeCotistas": quantidade_cotistas ,
+        #     "plFundo":  "plFundo" ,
+        #     "cotistas"
+        #
+        # }
     def criar_cotas(self , lista_cotas):
         # todo criar função para pegar a lógica das cotas
 
@@ -183,6 +189,10 @@ class Fundo5401():
             cotas_df_pre_ajustado.columns = ['tipo', 'cotatipo', 'valorCota' , 'qtdeCotas']
             cotas_df_pre_ajustado['vlCorrigido'] = str(cotas_df_pre_ajustado['qtdeCotas'].apply(float) * cotas_df_pre_ajustado['valorCota'].apply(float))
             elemento_cotista = self.criar_cotistas_unico(cotista)
+
+            print (cotas_df_pre_ajustado)
+
+
             cotas_xml_elemento = self.criar_cotas(cotas_df_pre_ajustado.to_dict("records"))
             elemento_cotista.append(cotas_xml_elemento)
             lista_de_cotas.append(elemento_cotista)
@@ -213,54 +223,60 @@ class Fundo5401():
 
         return lista_de_cotistas_cetip
 
+
+    def job_criar_cotista_unico(self , cotista , n_posicao ,  xml_cotistas ):
+
+        try:
+            cotas_df = n_posicao[n_posicao['cpfcnpjCotista'] == cotista['cpfcnpjCotista']]
+            cotas_df_pre_ajustado = cotas_df.groupby(['fundo', 'valor_cota', 'cotatipo'])[
+                ['vlCorrigido', 'qtCotas']].sum().reset_index()
+            cotas_df_pre_ajustado.columns = ['tipo', 'valorCota', 'tipoCota', 'vlCorrigido', 'qtdeCotas']
+
+            cotas_xml_elemento = ET.fromstring(cotas_df_pre_ajustado.to_xml(attr_cols=['tipoCota', 'qtdeCotas', 'valorCota'],
+                       index=False, root_name="cotas", row_name='cota', pretty_print=False))
+
+            # cotas_xml_elemento = self.criar_cotas(cotas_df_pre_ajustado.to_dict("records"))
+
+            elemento_cotista = self.criar_cotistas_unico(cotista)
+            elemento_cotista.append(cotas_xml_elemento)
+            xml_cotistas.append(elemento_cotista)
+
+        except Exception as e:
+            print (e ,  cotista)
+
+
+
+
     def transforma_posicao_posicao_informe(self):
         '''usa o df do fundo para transforma-lo no xml do 5401'''
         df_posicao = self.consultar_posicoes_jcot()[[ 'cd_cotista' , 'cpfcnpjCotista', 'fundo', 'valor_cota' , 'cotatipo' , 'vlCorrigido' , 'qtCotas']]
+
+        df_posicao['tipoCotista'] = df_posicao['cpfcnpjCotista'].apply(self.get_tipo_cotista_5401)
 
         n_posicao = df_posicao
 
         if not n_posicao.empty:
 
-
             total_cotistas = n_posicao['cpfcnpjCotista'].drop_duplicates().values
 
             xml_fundos = self.criar_fundo(self.CNPJ_EMISSOR , str(round(n_posicao['qtCotas'].sum() ,2)) , len(total_cotistas) ,  str(round(n_posicao['vlCorrigido'].sum() , 2)) )
 
-            # criação do elemento cotistas
+
+
             xml_cotistas = self.montar_cotistas()
-
-            # criar elemento cotista unico
-
 
             cotistas = n_posicao.drop_duplicates('cpfcnpjCotista')
 
-            cotista_cetip = cotistas[cotistas['cd_cotista'].isin(['09358105000191 ' , '09346601000125 '  ]) ]
+            job_cotista = partial(self.job_criar_cotista_unico ,  n_posicao=n_posicao , xml_cotistas=xml_cotistas)
 
-            for cotista in cotistas.to_dict('records'):
-                # todo incluir nesse ponto a consulta das cotas do respectivo cotista , para o respectivo fundo
-
-
-                if '9358105000191' not in cotista['cd_cotista'] and '9346601000125' not in  cotista['cd_cotista'] :
-                    #consulta das cotas do cotista  
-  
-                    cotas_df = n_posicao[n_posicao['cpfcnpjCotista'] == cotista['cpfcnpjCotista']]
-                    cotas_df_pre_ajustado  = cotas_df.groupby(['fundo', 'valor_cota' , 'cotatipo'])[['vlCorrigido' , 'qtCotas']].sum().reset_index()
-                    cotas_df_pre_ajustado.columns = ['tipo', 'valorCota' , 'cotatipo' , 'vlCorrigido' , 'qtdeCotas' ]
-                    cotas_xml_elemento = self.criar_cotas(cotas_df_pre_ajustado.to_dict("records"))
-                    elemento_cotista = self.criar_cotistas_unico(cotista)
-                    elemento_cotista.append(cotas_xml_elemento)
-
-                    xml_cotistas.append(elemento_cotista)
-
+            with ThreadPoolExecutor() as executor:
+                executor.map( job_cotista , cotistas.to_dict('records'))
 
             xml_fundos.append(xml_cotistas)
 
 
 
             return xml_fundos
-
-
-
 
 
     def ajustar_df_cetip(self , df_cetip):
--- ll.py
+++ ll.py
@@ -1,8 +1,24 @@
-from InformesLegais.Services.ServiceInvestidores import ServiceInvestidores
+from GERACAO_5401.Fundo5401 import Fundo5401
+from GERACAO_5401.Documento5401 import  Documento5401
+from GERACAO_5401.xml_5401 import XML_5401
+
+documento = Documento5401()
+gerador  = Fundo5401("43121036000136")
 
 
-invests = ServiceInvestidores()
+dados = gerador.transforma_posicao_posicao_informe()
 
 
-invests.consolidar_investidores()
+gerador.client['informes_legais']['fundos_consolidados'].insert(dados)
 
+
+
+#
+# fundo = gerador.transforma_posicao_posicao_informe()
+#
+# documento.adicionar_fundos(fundo)
+#
+# documento = documento.retornar_arquivo_5401_completo()
+# ajustador = XML_5401(documento)
+# ajustador.ajustar_arquivo_5401()
+# ajustador.reescrever_xml(f"teste.xml")
